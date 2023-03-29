9014306b57a1d69b1e6d3539721231d33f40fd57
Samuel <128160613+Samuel-industries@users.noreply.github.com>
2023-03-29 09:29:03 +0200

    Update Config.java

--- src/main/java/api/messages/Config.java
+++ src/main/java/api/messages/Config.java
@@ -1,30 +1,21 @@
 package api.messages;
-
 import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.List;
-
 import org.bukkit.configuration.file.FileConfiguration;
-
 import pl.samuel.skygen.core;
-
-
-
 
 public class Config
 {
-
-	
-
-	public static String DATABASE_MYSQL_HOST;
+    public static String DATABASE_MYSQL_HOST;
     public static int DATABASE_MYSQL_PORT;
     public static String DATABASE_MYSQL_USER;
     public static String DATABASE_MYSQL_PASS;
     public static String DATABASE_MYSQL_NAME;  
     public static String OTHER_redisurl;
     public static String OTHER_redispassword;  
-	public static String Weebhook_Url;
-	public static String Weebhook_IconUrl;
+    public static String Weebhook_Url;
+    public static String Weebhook_IconUrl;
     public static String CHAT_FORMAT_WLASCICIEL;
     public static String CHAT_FORMAT_HEADADMIN;
     public static String CHAT_FORMAT_ADMIN;
@@ -33,21 +24,20 @@ public class Config
     public static String CHAT_FORMAT_THELPER;
     public static String CHAT_FORMAT_PLAYER;
     public static String CHAT_FORMAT_GLOBAL;
-	public static String OTHER_lobbyname;
+    public static String OTHER_lobbyname;
     public static long OTHER_TURBOCOINS2;
     public static boolean OTHER_TURBOCOINS;
     public static int OTHER_SLOT;
     public static long OTHER_VOUCHERY;
     public static int OTHER_TURBOMNOZNIK;
     public static int OTHER_CHATSLOWMODE;    
-   	public static boolean OTHER_debug;
+    public static boolean OTHER_debug;
     public static int OTHER_BORDERWORLDRADIUS;
     public static boolean ENABLE_WHITELIST;
     public static boolean ENABLE_TELEPORTSRODEK;
     public static boolean ENABLE_TELEPORTLOBBY;
     public static boolean ENABLE_CHAT;
     public static boolean ENABLE_VIPCHAT;
- 
     public static String MSG_ERRORDONTHAVEPERMISSION;
     public static String MSG_USAGE;
     public static String MSG_FULLSERVER;
@@ -59,9 +49,7 @@ public class Config
         Config.DATABASE_MYSQL_PORT = 3306;
         Config.DATABASE_MYSQL_USER = "root";
         Config.DATABASE_MYSQL_PASS = "haslo";
-        Config.DATABASE_MYSQL_NAME = "skygen?useSSL=false";  
-
-        
+        Config.DATABASE_MYSQL_NAME = "skygen?useSSL=false";          
    ///chat
         Config.OTHER_CHATSLOWMODE = 10;   
         Config.CHAT_FORMAT_GLOBAL = "{GUILD}&7[&8{POINTS}&7] {PREFIX}{PLAYER} &8>> &r{SUFFIX}";        
@@ -73,8 +61,7 @@ public class Config
         Config.CHAT_FORMAT_HELPER = "&8&l[&9&lH&8&l] &9{PLAYER} &88> &b{MESSAGE}";
         Config.CHAT_FORMAT_THELPER = "&8&l[&9&lTH&8&l] &9{PLAYER} &88> &b{MESSAGE}";
         Config.CHAT_FORMAT_PLAYER = "&7{PLAYER} &88> &7{MESSAGE}";
-        ///tagi    
-     
+        ///tagi       
         Config.OTHER_TURBOCOINS = false;
         Config.OTHER_debug = false;
         Config.OTHER_TURBOCOINS2 = 0L;
@@ -83,7 +70,7 @@ public class Config
         Config.OTHER_SLOT = 500;
         Config.OTHER_BORDERWORLDRADIUS = 500;
         Config.OTHER_redisurl = "redis://127.0.0.1:6379";  
-        Config.OTHER_redispassword = "mtk11";  
+        Config.OTHER_redispassword = "jakieshaslo";  
         Config.OTHER_lobbyname = "lobby";
         Config.Weebhook_Url = "link";
         Config.Weebhook_IconUrl = "link.png";  
@@ -99,7 +86,6 @@ public class Config
         Config.WL_LIST = Collections.singletonList("nick");
         Config.WL_REASON = "&cNie jestes na whitelist!";   
     }
-
     public static void loadConfig() {
         try {
         	core.getPlugin().saveDefaultConfig();
