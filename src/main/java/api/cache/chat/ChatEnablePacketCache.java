af4e5bb4c007be53ba230cc7395a489c11db5bd0
Samuel <128160613+Samuel-industries@users.noreply.github.com>
2023-03-30 12:19:20 +0200

    Update ChatEnablePacketCache.java
    save

--- src/main/java/api/cache/chat/ChatEnablePacketCache.java
+++ src/main/java/api/cache/chat/ChatEnablePacketCache.java
@@ -14,6 +14,7 @@ public class ChatEnablePacketCache implements PacketListener<ChatEnablePacket> {
         System.out.println("dodano message do cache");
         String content = packet.getContent();
         Config.ENABLE_CHAT = true;
+        Config.save();
         Bukkit.broadcastMessage(ChatUtil.fixColor(content));            
     }
 }
